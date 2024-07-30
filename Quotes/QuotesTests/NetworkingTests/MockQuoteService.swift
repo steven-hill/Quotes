d050ee022ae14c090f8c2283a94c47338a8b9611
steven-hill <stevenhill15@icloud.com>
2024-07-30 22:40:28 +0100

    Have had to re-add mock JSON files, MockQuoteService, MockURLProtocol and QuoteServiceTests. Xcode was unable to find them / previous changes weren't showing.

--- Quotes/QuotesTests/NetworkingTests/MockQuoteService.swift
+++ Quotes/QuotesTests/NetworkingTests/MockQuoteService.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+@testable import Quotes
+
+final class MockQuoteService: QuoteServiceProtocol {
+    
+    var quoteResponse: QuoteServiceResult = []
+    let mockURL = URL(string: "https://zenquotes.io/api/today")
+    var shouldSucceed: Bool = true
+    
+    enum MockQuoteServiceError: Error, LocalizedError {
+        case networkConnectionOffline
+        case invalidURL
+        case invalidStatusCode
+        case invalidData
+        case unknown(Error)
+    }
+    
+    var bundle: Bundle {
+        return Bundle(for: type(of: self))
+    }
+    
+    func readMockQuoteResponseJsonFile() -> QuoteServiceResult {
+        guard let path = bundle.url(forResource: "QuoteResponse", withExtension: "json") else {
+            fatalError("Failed to load the mock quote response JSON file.")
+        }
+        
+        do {
+            let data = try Data(contentsOf: path)
+            let decodedObject = try JSONDecoder().decode(QuoteServiceResult.self, from: data)
+            return decodedObject
+        } catch {
+            print("\(error)")
+            fatalError("QuoteResponseJson decoding failed.")
+        }
+    }
+    
+    func fetchQuoteOfTheDay() async throws -> QuoteServiceResult {
+        if shouldSucceed {
+            quoteResponse = readMockQuoteResponseJsonFile()
+            return quoteResponse
+        } else {
+            throw NSError(domain: "com.example.app", code: 0, userInfo: [NSLocalizedDescriptionKey: "Mock error"])
+        }
+    }
+}
+
+extension MockQuoteService.MockQuoteServiceError: Equatable {
+    static func == (lhs: MockQuoteService.MockQuoteServiceError, rhs: MockQuoteService.MockQuoteServiceError) -> Bool {
+        switch(lhs, rhs) {
+        case(.networkConnectionOffline, .networkConnectionOffline):
+            return true
+        case(.invalidURL, .invalidURL):
+            return true
+        case(.invalidStatusCode, .invalidStatusCode):
+            return true
+        case(.invalidData, .invalidData):
+            return true
+        case(.unknown(let lhsType), .unknown(let rhsType)):
+            return lhsType.localizedDescription == rhsType.localizedDescription
+        default:
+            return false
+        }
+    }
+}
--- Quotes/QuotesTests/NetworkingTests/QuoteServiceTests.swift
+++ Quotes/QuotesTests/NetworkingTests/QuoteServiceTests.swift
@@ -6,30 +6,124 @@
 //
 
 import XCTest
+@testable import Quotes
 
 final class QuoteServiceTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    private var url: URL!
+    
+    lazy var session: URLSession = {
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
+        return URLSession(configuration: configuration)
+    }()
+    
+    lazy var api: QuoteService = {
+        QuoteService(session: session)
+    }()
+    
+    override func setUp() {
+        url = URL(string: "https://zenquotes.io/api/today")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        MockURLProtocol.requestHandler = nil
+        url = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_FetchQuoteOfTheDay_URL_IsValid_And_ReturnsOneResult() async throws {
+        let mockData =
+            """
+            [ {"q":"When you want to be honored by others, you learn to honor them first.","a":"Sathya Sai Baba","h":"<blockquote>&ldquo;When you want to be honored by others, you learn to honor them first.&rdquo; &mdash; <footer>Sathya Sai Baba</footer></blockquote>"} ]
+            """.data(using: .utf8)!
+        
+        MockURLProtocol.requestHandler = { request in
+            XCTAssertEqual(request.url?.absoluteString, self.url.absoluteString)
+            
+            let response = HTTPURLResponse(
+                url: request.url!,
+                statusCode: 200,
+                httpVersion: nil,
+                headerFields: nil
+            )!
+            return (response, mockData)
+        }
+        let result = try await api.fetchQuoteOfTheDay()
+        XCTAssertFalse(result.isEmpty)
+        XCTAssertEqual(result.count, 1)
+        //XCTAssertEqual(result.count, mockData.count)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_FetchQuoteOfTheDay_DecodesCorrectly() async throws {
+        let data =
+            """
+            [ {"q":"When you want to be honored by others, you learn to honor them first.","a":"Sathya Sai Baba","h":"<blockquote>&ldquo;When you want to be honored by others, you learn to honor them first.&rdquo; &mdash; <footer>Sathya Sai Baba</footer></blockquote>"} ]
+            """.data(using: .utf8)!
+        
+        MockURLProtocol.requestHandler = { _ in
+            let response = HTTPURLResponse(
+                url: self.url,
+                statusCode: 200,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            return (response!, data)
+        }
+        
+        let mockQuoteService = MockQuoteService()
+        let decodedData = mockQuoteService.readMockQuoteResponseJsonFile()
+        print(decodedData)
+        let result = try await api.fetchQuoteOfTheDay()
+        
+        XCTAssertEqual(decodedData, result)
+    }
+    
+    func test_Response_IsValid_And_Has_InvalidStatusCode() async {
+        let invalidStatusCode = 400
+        let data = Data()
+        
+        MockURLProtocol.requestHandler = { _ in
+            let response = HTTPURLResponse(
+                url: self.url,
+                statusCode: 400,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            return (response!, data)
+        }
+        
+        do {
+            let _ = try await api.fetchQuoteOfTheDay()
+        } catch {
+            guard let networkError = error as? QuoteService.QuoteServiceError else {
+                XCTFail("Wrong error type, expecting QuoteService.QuoteServiceError")
+                return
+            }
+            XCTAssertEqual(networkError, QuoteService.QuoteServiceError.invalidStatusCode(statusCode: invalidStatusCode), "Expecting a networking error that throws invalid status code 400")
         }
     }
-
+    
+    func test_Response_IsValid_With_InvalidJSON() {
+        let data = "Invalid Data".data(using: .utf8)!
+        
+        MockURLProtocol.requestHandler = { _ in
+            let response = HTTPURLResponse(
+                url: self.url,
+                statusCode: 200,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            return (response!, data)
+        }
+        
+        do {
+            _ = try api.decoder.decode(QuoteServiceResult.self, from: data)
+        } catch {
+            print(error.localizedDescription)
+            if error is QuoteService.QuoteServiceError { // Add breakpoint on this line to see decoding error in console
+                XCTFail("The error should be a JSON decoding error.")
+            }
+        }
+    }
 }
