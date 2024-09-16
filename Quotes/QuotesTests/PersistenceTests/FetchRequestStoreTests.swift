e1f3827c569ca4abb321006647d339a1a42be04c
steven-hill <stevenhill15@icloud.com>
2024-09-16 22:03:55 +0100

    Fixed a typo. Fixed typos in two test messages.

--- Quotes/QuotesTests/PersistenceTests/FetchRequestStoreTests.swift
+++ Quotes/QuotesTests/PersistenceTests/FetchRequestStoreTests.swift
@@ -24,7 +24,7 @@ final class FetchRequestStoreTests: XCTestCase {
         sut.tryFetch()
         
         XCTAssertEqual(sut.fetchState, .success(data: sut.savedQuotes), "Successful fetch should return FetchState success case.")
-        XCTAssertEqual(sut.fetchRequestHasError, false, "'fetchRequestError' should remain false in success case.")
+        XCTAssertEqual(sut.fetchRequestHasError, false, "'fetchRequestHasError' should remain false in success case.")
     }
     
     func test_tryFetchFailure_returnsFetchStateFailure() {
@@ -33,7 +33,7 @@ final class FetchRequestStoreTests: XCTestCase {
         sut.tryFetch()
         
         XCTAssertEqual(sut.fetchState, .failure(error: MockFetchRequestStore.MockFetchError.fetchFailure), "Fetch failure should return FetchState failure case.")
-        XCTAssertEqual(sut.fetchRequestHasError, true, "'fetchRequestError' should be true in failure case.")
+        XCTAssertEqual(sut.fetchRequestHasError, true, "'fetchRequestHasError' should be true in failure case.")
     }
     
     func test_tryFetch_returnsEmptyArrayWhenStoreIsEmpty() {
