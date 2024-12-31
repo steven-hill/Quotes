27fcd774bd41066007d8b8acf1460e409c8b1688
steven-hill <stevenhill15@icloud.com>
2024-12-31 17:49:57 +0000

    Local notifications unit test file. Setup a clean unit test file.

---
+++ Quotes/QuotesTests/LocalNotificationsTests/LocalNotificationManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationManagerTests.swift
+//  QuotesTests
+//
+//  Created by Steven Hill on 31/12/2024.
+//
+
+import XCTest
+
+final class LocalNotificationManagerTests: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+}
