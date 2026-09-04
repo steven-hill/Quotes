e783640f8f6d84533e5b1a5d0c7a525dd62e543b
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-04 13:59:21 +0100

    delete `UserDefaultsProviding`

--- Quotes/Quotes/Appearance/AppearanceManager.swift
+++ Quotes/Quotes/Appearance/AppearanceManager.swift
@@ -7,13 +7,6 @@
 
 import SwiftUI
 
-protocol UserDefaultsProviding {
-    func integer(forKey defaultName: String) -> Int
-    func set(_ value: Int, forKey defaultName: String)
-}
-
-extension UserDefaults: UserDefaultsProviding {}
-
 enum Appearance: Int {
     case unspecified
     case light
