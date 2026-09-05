95e192adec23293ca89a510b7e2e6d32f2708988
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-05 19:23:29 +0100

    add more cases to the error, add user facing descriptions, and mapping logic for `URLError` and `DecodingError`

--- Quotes/Quotes/Networking/NetworkError.swift
+++ Quotes/Quotes/Networking/NetworkError.swift
@@ -7,12 +7,65 @@
 
 import Foundation
 
-enum NetworkError: Error, LocalizedError {
+enum NetworkError: Error {
     case networkConnectionOffline
+    case networkConnectionLost
+    case networkTimeout
     case invalidURL
+    case invalidResponse
     case invalidStatusCode(statusCode: Int)
     case invalidData
-    case unknown(Error)
+    case unknown
+}
+
+// MARK: - User Facing Descriptions
+extension NetworkError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .networkConnectionOffline:
+            return "No network connection."
+        case .networkConnectionLost:
+            return "Network connection was lost."
+        case .networkTimeout:
+            return "Network connection timed out."
+        case .invalidURL, .invalidResponse, .invalidData, .unknown:
+            return "Something went wrong."
+        case .invalidStatusCode(let statusCode):
+            return "Invalid response from server (Status: \(statusCode))."
+        }
+    }
+}
+
+// MARK: - Mapping Logic
+extension NetworkError {
+    /// Maps generic network or system errors into a `NetworkError`.
+    init(from error: Error) {
+        if let apiError = error as? NetworkError {
+            self = apiError
+            return
+        }
+        
+        if let urlError = error as? URLError {
+            switch urlError.code {
+            case .notConnectedToInternet:
+                self = .networkConnectionOffline
+            case .networkConnectionLost:
+                self = .networkConnectionLost
+            case .timedOut:
+                self = .networkTimeout
+            default:
+                self = .unknown
+            }
+            return
+        }
+        
+        if error is DecodingError {
+            self = .invalidData
+            return
+        }
+        
+        self = .unknown
+    }
 }
 
 extension NetworkError: Equatable {
@@ -26,8 +79,8 @@ extension NetworkError: Equatable {
             return lhsType == rhsType
         case(.invalidData, .invalidData):
             return true
-        case(.unknown(let lhsType), .unknown(let rhsType)):
-            return lhsType.localizedDescription == rhsType.localizedDescription
+        case(.unknown, .unknown):
+            return true
         default:
             return false
         }
