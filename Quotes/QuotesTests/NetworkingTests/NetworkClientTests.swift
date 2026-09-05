119bf060a5de88460a119c78512b8f263bf059b2
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-05 20:28:18 +0100

    add test file

---
+++ Quotes/QuotesTests/NetworkingTests/NetworkClientTests.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkClientTests.swift
+//  QuotesTests
+//
+//  Created by Steven Hill on 05/09/2026.
+//
+
+import Testing
+
+struct NetworkClientTests {
+
+    @Test func doNothing() async throws {
+    }
+}
