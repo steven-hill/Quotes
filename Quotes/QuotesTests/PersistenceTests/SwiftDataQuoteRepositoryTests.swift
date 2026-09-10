929d7cc4e965ed796e4565d0abb024db48dd1b8a
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-10 20:02:20 +0100

    add unit test file for the repository

---
+++ Quotes/QuotesTests/PersistenceTests/SwiftDataQuoteRepositoryTests.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftDataQuoteRepositoryTests.swift
+//  QuotesTests
+//
+//  Created by Steven Hill on 10/09/2026.
+//
+
+import Testing
+
+struct SwiftDataQuoteRepositoryTests {
+
+    @Test func doNothing() {
+    }
+}
