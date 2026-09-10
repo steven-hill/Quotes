372d2711d83af902e2c1ce2ad0347a47fc45bb08
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-10 11:32:17 +0100

    create view model file and its unit test file

---
+++ Quotes/Quotes/ViewModels/SavedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SavedViewModel.swift
+//  Quotes
+//
+//  Created by Steven Hill on 10/09/2026.
+//
+
+import Foundation
+
+final class SavedViewModel {
+    
+}
---
+++ Quotes/QuotesTests/ViewModelTests/SavedViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  SavedViewModelTests.swift
+//  QuotesTests
+//
+//  Created by Steven Hill on 10/09/2026.
+//
+
+import Testing
+
+struct SavedViewModelTests {
+
+    @Test func doNothing() {
+    }
+}
