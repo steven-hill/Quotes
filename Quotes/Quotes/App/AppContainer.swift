7adf4422890d5cf85ee45cf1ac2d254664b5a6ad
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-13 18:10:32 +0100

    add function to make an in-memory model container for previews

--- Quotes/Quotes/App/AppContainer.swift
+++ Quotes/Quotes/App/AppContainer.swift
@@ -35,4 +35,29 @@ final class AppContainer {
         }
         self.networkClient = NetworkClient()
     }
+    
+    //MARK: - In-memory Model Container
+    static func makePreviewContainer(withSampleData: Bool = true) -> AppContainer {
+        let container = AppContainer(isInMemoryOnly: true)
+        if withSampleData {
+            let sampleQuotes = [
+                Quote(
+                    id: nil,
+                    text: "The only limit to our realization of tomorrow is our doubts of today.",
+                    author: "FDR",
+                    date: Date()
+                ),
+                Quote(
+                    id: nil,
+                    text: "Be yourself; everyone else is already taken.",
+                    author: "Oscar Wilde",
+                    date: Date()
+                )
+            ]
+            for quote in sampleQuotes {
+                try? container.swiftDataQuoteRepository.add(quote)
+            }
+        }
+        return container
+    }
 }
