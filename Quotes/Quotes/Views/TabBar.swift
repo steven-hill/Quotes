889169eea13996f910625c55914322d93c25d8ab
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-13 17:48:44 +0100

    `TabBar` injects `QuoteRepository` into `SavedView`

--- Quotes/Quotes/Views/TabBar.swift
+++ Quotes/Quotes/Views/TabBar.swift
@@ -46,7 +46,7 @@ struct TabBar: View {
                 .onAppear { selectedTab = .home }
                 .tag(Tab.home)
             
-            SavedView()
+            SavedView(quoteRepository: appContainer.swiftDataQuoteRepository)
                 .tabItem {
                     Label("Saved", systemImage: selectedTab == .saved ? "bookmark.fill" : "bookmark")
                         .environment(\.symbolVariants, selectedTab == .saved ? .fill : .none)
