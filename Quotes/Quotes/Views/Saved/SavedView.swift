ccdbc7369a55366359b9b840c9fb7dd744b21ba5
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-13 17:43:47 +0100

    `SavedView` takes in `QuoteRepository` and injects it into the view model

--- Quotes/Quotes/Views/Saved/SavedView.swift
+++ Quotes/Quotes/Views/Saved/SavedView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct SavedView: View {
     
@@ -21,10 +22,20 @@ struct SavedView: View {
     @State private var alertMessage = ""
     @State private var showDeleteQuoteAlert: Bool = false
     @State private var quoteToDelete: SavedQuote?
+    @State private var savedVM: SavedViewModel
     
     // MARK: - Constants
     let deleteQuoteAlertMessage = "This action can't be undone."
     
+    // MARK: - Dependency
+    private let quoteRepository: QuoteRepository
+    
+    // MARK: - Initialisation
+    init(quoteRepository: QuoteRepository) {
+        self.quoteRepository = quoteRepository
+        _savedVM = State(initialValue: SavedViewModel(repository: quoteRepository))
+    }
+        
     // MARK: - Body
     var body: some View {
         NavigationStack {
@@ -147,6 +158,13 @@ extension SavedView {
 }
 
 #Preview {
-    SavedView()
+    let configuration = ModelConfiguration(
+        isStoredInMemoryOnly: true
+    )
+    let modelContainer = try! ModelContainer(
+        for: PersistedQuote.self,
+        configurations: configuration
+    )
+    SavedView(quoteRepository: SwiftDataQuoteRepository(container: modelContainer))
         .environmentObject(FetchRequestStore.preview)
 }
