39f2949231929ef8b601e330e9d2e3410110f92f
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-16 23:03:06 +0100

    add unit test file

---
+++ Quotes/QuotesTests/AppTests/AppContainerTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppContainerTests.swift
+//  QuotesTests
+//
+//  Created by Steven Hill on 16/09/2026.
+//
+
+import Testing
+
+struct AppContainerTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
