d1a759e828d3f31c7592eb1b2ea68a132288177d
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-18 22:02:19 +0100

    add test file

---
+++ Quotes/QuotesTests/ViewModelTests/ReflectOnQuoteViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  ReflectOnQuoteViewModelTests.swift
+//  QuotesTests
+//
+//  Created by Steven Hill on 18/09/2026.
+//
+
+import Testing
+
+struct ReflectOnQuoteViewModelTests {
+
+    @Test func doNothing() {
+    }
+}
