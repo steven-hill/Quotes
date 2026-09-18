9d331ee1489209a2b54216dde4c26274cfbfd874
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-18 22:07:07 +0100

    add `ReflectOnQuoteViewModel`

---
+++ Quotes/Quotes/ViewModels/ReflectOnQuoteViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ReflectOnQuoteViewModel.swift
+//  Quotes
+//
+//  Created by Steven Hill on 18/09/2026.
+//
+
+import Foundation
+
+@Observable
+final class ReflectOnQuoteViewModel {
+    
+}
--- Quotes/QuotesTests/ViewModelTests/ReflectOnQuoteViewModelTests.swift
+++ Quotes/QuotesTests/ViewModelTests/ReflectOnQuoteViewModelTests.swift
@@ -6,9 +6,12 @@
 //
 
 import Testing
+@testable import Quotes
 
 struct ReflectOnQuoteViewModelTests {
 
-    @Test func doNothing() {
+    @Test("VM's properties are set correctly on init")
+    func reflectOnQuoteViewModel_onInit_propertiesAreSetCorrectly() {
+        let sut = ReflectOnQuoteViewModel()
     }
 }
