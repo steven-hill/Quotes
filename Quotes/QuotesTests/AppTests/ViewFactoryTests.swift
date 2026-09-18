2445da8aec1fba7717614d5b8aad24b3a5fd010d
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-18 17:18:20 +0100

    update access control on mocks

--- Quotes/QuotesTests/AppTests/ViewFactoryTests.swift
+++ Quotes/QuotesTests/AppTests/ViewFactoryTests.swift
@@ -31,39 +31,40 @@ struct ViewFactoryTests {
         
         #expect(mockContainer.didAccessQuoteRepository == true, "Should have got the quote repository instance from the dependencies.")
     }
-}
+    
+    //MARK: - Mocks
+    private final class MockDependencyContainer: AppDependencyContaining {
+        
+        // MARK: - Properties
+        private let mockQuoteRepository: QuoteRepository
+        private let mockNetworkClient: Networking
+        
+        // MARK: - Tracking States
+        var didAccessQuoteRepository = false
+        var didAccessNetworkClient = false
+        var quoteRepository: QuoteRepository {
+            didAccessQuoteRepository = true
+            return mockQuoteRepository
+        }
+        
+        var networkClient: Networking {
+            didAccessNetworkClient = true
+            return mockNetworkClient
+        }
+        
+        // MARK: - Initialisation
+        init(
+            quoteRepository: QuoteRepository = MockQuoteRepository(),
+            networkClient: Networking = NetworkClient(session: MockNetworkSession())
+        ) {
+            self.mockQuoteRepository = quoteRepository
+            self.mockNetworkClient = networkClient
+        }
+    }
 
-final class MockDependencyContainer: AppDependencyContaining {
-    
-    // MARK: - Properties
-    private let mockQuoteRepository: QuoteRepository
-    private let mockNetworkClient: Networking
-    
-    // MARK: - Tracking States
-    var didAccessQuoteRepository = false
-    var didAccessNetworkClient = false
-    var quoteRepository: QuoteRepository {
-        didAccessQuoteRepository = true
-        return mockQuoteRepository
-    }
-    
-    var networkClient: Networking {
-        didAccessNetworkClient = true
-        return mockNetworkClient
-    }
-    
-    // MARK: - Initialisation
-    init(
-        quoteRepository: QuoteRepository = MockQuoteRepository(),
-        networkClient: Networking = NetworkClient(session: MockNetworkSession())
-    ) {
-        self.mockQuoteRepository = quoteRepository
-        self.mockNetworkClient = networkClient
-    }
-}
-
-struct MockNetworkSession: NetworkSession {
-    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
-        return (Data(), URLResponse())
+    private struct MockNetworkSession: NetworkSession {
+        func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+            return (Data(), URLResponse())
+        }
     }
 }
