e66e259b5b60d51a189d662fd038ea00bf02fb62
Steven Hill <98730693+steven-hill@users.noreply.github.com>
2026-09-18 16:58:32 +0100

    view factory creates `QuoteOfTheDayView` with network client instance from app dependencies

--- Quotes/Quotes/App/ViewFactory.swift
+++ Quotes/Quotes/App/ViewFactory.swift
@@ -5,13 +5,21 @@
 //  Created by Steven Hill on 18/09/2026.
 //
 
-import Foundation
+import SwiftUI
 
 /// Manages `SwiftUI` presentation dependencies.
 final class ViewFactory {
+    
+    //MARK: - Dependency
     private let dependencies: AppDependencyContaining
     
+    //MARK: - Initialisation
     init(dependencies: AppDependencyContaining) {
         self.dependencies = dependencies
+    }
+    
+    //MARK: - View Creation Method
+    func makeQuoteOfTheDayView() -> some View {
+        QuoteOfTheDayView(networkClient: dependencies.networkClient)
     }
 }
--- Quotes/QuotesTests/AppTests/ViewFactoryTests.swift
+++ Quotes/QuotesTests/AppTests/ViewFactoryTests.swift
@@ -6,27 +6,54 @@
 //
 
 import Testing
+import Foundation
 @testable import Quotes
 
 @MainActor
 struct ViewFactoryTests {
 
-    @Test("View factory successfully accesses network client for QuoteOfTheDayView")
-    func viewFactory_makeQuoteOfTheDayView_pullsCorrectDependencyFromAppContainer() {
+    @Test("View factory successfully accesses network client for `QuoteOfTheDayView`")
+    func viewFactory_makeQuoteOfTheDayView_pullsCorrectDependencyFromAppDependencies() {
         let mockDependencyContainer = MockDependencyContainer()
         let sut = ViewFactory(dependencies: mockDependencyContainer)
+        
+        _ = sut.makeQuoteOfTheDayView()
+                
+        #expect(mockDependencyContainer.didAccessNetworkClient == true, "Should have got the network client instance from the dependencies.")
     }
 }
 
 final class MockDependencyContainer: AppDependencyContaining {
-    let quoteRepository: QuoteRepository
-    let networkClient: Networking
     
+    // MARK: - Properties
+    private let mockQuoteRepository: QuoteRepository
+    private let mockNetworkClient: Networking
+    
+    // MARK: - Tracking States
+    var didAccessQuoteRepository = false
+    var didAccessNetworkClient = false
+    var quoteRepository: QuoteRepository {
+        didAccessQuoteRepository = true
+        return mockQuoteRepository
+    }
+    
+    var networkClient: Networking {
+        didAccessNetworkClient = true
+        return mockNetworkClient
+    }
+    
+    // MARK: - Initialisation
     init(
         quoteRepository: QuoteRepository = MockQuoteRepository(),
-        networkClient: Networking = NetworkClient(session: StubNetworkSession())
+        networkClient: Networking = NetworkClient(session: MockNetworkSession())
     ) {
-        self.quoteRepository = quoteRepository
-        self.networkClient = networkClient
+        self.mockQuoteRepository = quoteRepository
+        self.mockNetworkClient = networkClient
+    }
+}
+
+struct MockNetworkSession: NetworkSession {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        return (Data(), URLResponse())
     }
 }
